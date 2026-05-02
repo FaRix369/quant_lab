0592f9ca72460b743502c82d2799ccc1654cc3b1
Farix369 <farix369125@gmail.com>
2026-05-02 09:01:59 +0000

    Add returns_correlation: correlation matrix over log returns

---
+++ analysis/correlation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from database.querys import q_returns_indexed
+from feature_engineering.returns import log_return
+
+def returns_correlation(assets):
+    df = pd.DataFrame()
+    for ticker, asset in assets.items():
+        df[ticker] = log_return(asset).rename(ticker)
+    correlation = df.corr()
+    return correlation
+
+
+if __name__ == "__main__":
+    assets={
+        'ASML' : q_returns_indexed('ASML', '1y'),
+        'SPY' : q_returns_indexed('SPY', '1y')
+    }
+
+    correlation = returns_correlation(assets)
+    print(correlation) 
