a3a6a78950ab68f571e234fa38974794f88cb70d
Farix369 <farix369125@gmail.com>
2026-05-10 21:20:06 +0000

    Add volatility_chart

--- visualization/feature_charts.py
+++ visualization/feature_charts.py
@@ -1,4 +1,6 @@
+from matplotlib.ticker import PercentFormatter
 from feature_engineering.returns import log_return
+from feature_engineering.volatility import rolling_volatility
 from pipelines.stock_collector.collector import collect
 import matplotlib
 matplotlib.use('TkAgg')
@@ -15,10 +17,23 @@ def returns_chart(date, returns):
     plt.show()
     return fig
 
+def volatility_chart(date, volatility):
+    fig, ax = plt.subplots()
+    ax.plot(date, volatility, label='Volatility Price')
+    ax.yaxis.set_major_formatter(PercentFormatter(xmax=1))
+    ax.legend()
+    ax.set_title('Volatility')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Volatility')
+    plt.show()
+    return fig
+
 if __name__ == "__main__":
     df_asml = collect('ASML', '6mo')
     date = df_asml['date']
     volume = df_asml['volume']
     returns = log_return(df_asml)
+    volatility = rolling_volatility(returns, 20)
 
-    retu = returns_chart(date, returns)
+    returns_c = returns_chart(date, returns)
+    volatility_c = volatility_chart(date, volatility)
