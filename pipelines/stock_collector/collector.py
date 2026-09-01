8338353024f30d19f3cab4a50c681bbf518d942a
fbalbinotdoif <farix369125@gmail.com>
2026-09-01 05:07:58 +0200

    Remove legacy CLI import from collector

--- pipelines/stock_collector/collector.py
+++ pipelines/stock_collector/collector.py
@@ -1,7 +1,6 @@
 from pipelines.stock_collector.api_client import history_price
 from pipelines.stock_collector.transform import transform_data
 from pipelines.stock_collector.db_writer import writer
-from cli.cli import get_ticker_period
 
 def collect (ticker, period):
     print(f'Collecting {ticker} from {period}')
@@ -13,8 +12,3 @@ def collect (ticker, period):
         df = transform_data(data, ticker)
         writer(df)
         return df
-
-
-if __name__ == '__main__':
-    ticker, period = get_ticker_period()
-    collect(ticker, period)
